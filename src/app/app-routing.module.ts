9dab5c3e6d74fef79c388b2b4b267d0bbd24735c
Rahul Bhilesha <rahulbhilesha077@gmail.com>
2022-04-29 10:54:34 +0530

    Initial commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./components/authentication/authentication.module').then(m => m.AuthenticationModule) },
+  { path: '', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
